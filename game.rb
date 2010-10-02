20b5618dd2b1fb9eecd2a9c9f6b5b84c0a64fac1
thorncp <thorncp@gmail.com>
2010-10-01 17:22:29 -0800

    removing all movement code from the player. writing this myself will be a good way to learn what's going on and i can design it how i see fit

--- game.rb
+++ game.rb
@@ -10,16 +10,18 @@ class Game < Gosu::Window
     # The scrolling position is stored as top left corner of the screen.
     @camera_x = @camera_y = 0
   end
+  
   def update
     move_x = 0
     move_x -= 5 if button_down? Gosu::KbLeft
     move_x += 5 if button_down? Gosu::KbRight
     @cptn.update(move_x)
-    @cptn.collect_gems(@map.gems)
+    
     # Scrolling follows player
     @camera_x = [[@cptn.x - 320, 0].max, @map.width * 50 - 640].min
     @camera_y = [[@cptn.y - 240, 0].max, @map.height * 50 - 480].min
   end
+  
   def draw
     @sky.draw 0, 0, 0
     translate(-@camera_x, -@camera_y) do
@@ -27,8 +29,8 @@ class Game < Gosu::Window
       @cptn.draw
     end
   end
+  
   def button_down(id)
-    if id == Gosu::KbUp then @cptn.try_to_jump end
     if id == Gosu::KbEscape then close end
   end
 end
--- player.rb
+++ player.rb
@@ -2,81 +2,32 @@ class Player
   attr_reader :x, :y
 
   def initialize(window, x, y)
+    @size = 50
     @x, @y = x, y
     @dir = :left
     @vy = 0 # Vertical velocity
     @map = window.map
     # Load all animation frames
     @standing, @walk1, @walk2, @jump =
-      *Gosu::Image.load_tiles(window, "media/CptnRuby.png", 50, 50, false)
+      *Gosu::Image.load_tiles(window, "media/CptnRuby.png", @size, @size, false)
     # This always points to the frame that is currently drawn.
     # This is set in update, and used in draw.
     @cur_image = @standing    
   end
   
   def draw
-    # Flip vertically when facing to the left.
-    if @dir == :left then
-      offs_x = -25
-      factor = 1.0
-    else
-      offs_x = 25
-      factor = -1.0
+    offs_x = 25
+    factor = -1.0
+    
+    if @dir == :left
+      offs_x *= -1
+      factor *= -1
     end
-    @cur_image.draw(@x + offs_x, @y - 49, 0, factor, 1.0)
-  end
-  
-  # Could the object be placed at x + offs_x/y + offs_y without being stuck?
-  def would_fit(offs_x, offs_y)
-    # Check at the center/top and center/bottom for map collisions
-    not @map.solid?(@x + offs_x, @y + offs_y) and
-      not @map.solid?(@x + offs_x, @y + offs_y - 45)
+    
+    @cur_image.draw(@x + offs_x, @y - @size - 1, 0, factor, 1.0)
   end
   
   def update(move_x)
-    # Select image depending on action
-    if (move_x == 0)
-      @cur_image = @standing
-    else
-      @cur_image = (Gosu::milliseconds / 175 % 2 == 0) ? @walk1 : @walk2
-    end
-    if (@vy < 0)
-      @cur_image = @jump
-    end
     
-    # Directional walking, horizontal movement
-    if move_x > 0 then
-      @dir = :right
-      move_x.times { if would_fit(1, 0) then @x += 1 end }
-    end
-    if move_x < 0 then
-      @dir = :left
-      (-move_x).times { if would_fit(-1, 0) then @x -= 1 end }
-    end
-
-    # Acceleration/gravity
-    # By adding 1 each frame, and (ideally) adding vy to y, the player's
-    # jumping curve will be the parabole we want it to be.
-    @vy += 1
-    # Vertical movement
-    if @vy > 0 then
-      @vy.times { if would_fit(0, 1) then @y += 1 else @vy = 0 end }
-    end
-    if @vy < 0 then
-      (-@vy).times { if would_fit(0, -1) then @y -= 1 else @vy = 0 end }
-    end
-  end
-  
-  def try_to_jump
-    if @map.solid?(@x, @y + 1) then
-      @vy = -20
-    end
-  end
-  
-  def collect_gems(gems)
-    # Same as in the tutorial game.
-    gems.reject! do |c|
-      (c.x - @x).abs < 50 and (c.y - @y).abs < 50
-    end
   end
 end
