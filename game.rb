f10ff1fcb9f9ebee84ac5abb480f9a1b55d81fbf
thorncp <thorncp@gmail.com>
2010-10-01 17:56:09 -0800

    add x axis movement to player

--- game.rb
+++ game.rb
@@ -2,11 +2,13 @@ class Game < Gosu::Window
   attr_reader :map
 
   def initialize
-    super(640, 480, false)
+    @resolution_x = 640
+    @resolution_y = 480
+    super(@resolution_x, @resolution_y, false)
     self.caption = "Cptn. Ruby"
     @sky = Gosu::Image.new(self, "media/Space.png", true)
     @map = Map.new(self, "media/CptnRuby Map.txt")
-    @cptn = Player.new(self, 400, 100)
+    @player = Player.new(self, 400, 100)
     # The scrolling position is stored as top left corner of the screen.
     @camera_x = @camera_y = 0
   end
@@ -15,18 +17,19 @@ class Game < Gosu::Window
     move_x = 0
     move_x -= 5 if button_down? Gosu::KbLeft
     move_x += 5 if button_down? Gosu::KbRight
-    @cptn.update(move_x)
+    
+    @player.update(move_x)
     
     # Scrolling follows player
-    @camera_x = [[@cptn.x - 320, 0].max, @map.width * 50 - 640].min
-    @camera_y = [[@cptn.y - 240, 0].max, @map.height * 50 - 480].min
+    @camera_x = [[@player.x - @resolution_x / 2, 0].max, @map.width * 50 - @resolution_x].min
+    @camera_y = [[@player.y - @resolution_y / 2, 0].max, @map.height * 50 - @resolution_y].min
   end
   
   def draw
     @sky.draw 0, 0, 0
     translate(-@camera_x, -@camera_y) do
       @map.draw
-      @cptn.draw
+      @player.draw
     end
   end
   
--- player.rb
+++ player.rb
@@ -1,33 +1,70 @@
 class Player
   attr_reader :x, :y
+  
+  ImageFileName = "media/CptnRuby.png"
+  ImageSize = 50
+  BobbleInterval = 200
 
   def initialize(window, x, y)
-    @size = 50
     @x, @y = x, y
     @dir = :left
-    @vy = 0 # Vertical velocity
+    
     @map = window.map
+    
     # Load all animation frames
-    @standing, @walk1, @walk2, @jump =
-      *Gosu::Image.load_tiles(window, "media/CptnRuby.png", @size, @size, false)
+    images = Gosu::Image.load_tiles(window, ImageFileName, ImageSize, ImageSize, false)
+    @images = Hash[[:standing, :bobble1, :bobble2, :jump].zip(images)]
+    
     # This always points to the frame that is currently drawn.
     # This is set in update, and used in draw.
-    @cur_image = @standing    
+    @cur_image = @images[:standing]
   end
   
   def draw
-    offs_x = 25
-    factor = -1.0
-    
     if @dir == :left
-      offs_x *= -1
-      factor *= -1
+      offs_x = -ImageSize / 2
+      factor = 1.0
+    else
+      offs_x = ImageSize / 2
+      factor = -1.0
     end
     
-    @cur_image.draw(@x + offs_x, @y - @size - 1, 0, factor, 1.0)
+    @cur_image.draw(@x + offs_x, @y - ImageSize - 1, 0, factor, 1.0)
   end
   
-  def update(move_x)
+  def update(delta_x)
+    @moving = false
     
+    unless delta_x.zero?
+      # determine which direction we're facing
+      @dir = delta_x > 0 ? :right : :left
+      
+      # make the move
+      move delta_x, 0
+    end
+    
+    if moving?
+      # bobble the sprite
+      @cur_image = bobble? ? @images[:bobble1] : @images[:bobble2]
+    else
+      # stand still
+      @cur_image = @images[:standing]
+    end
+  end
+  
+  def moving?
+    @moving
+  end
+  
+  def bobble?
+    (Gosu::milliseconds / BobbleInterval).even?
+  end
+  
+  def move(delta_x = 0, delta_y = 0)
+    unless @map.solid?(@x + delta_x, @y + delta_y)
+      @x += delta_x
+      @y += delta_y
+      @moving ||= true
+    end
   end
 end
